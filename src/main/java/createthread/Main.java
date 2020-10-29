1e6ee6d32c39670f0fc6e6c0a5ad2d6da707de2a
Mingyu <qiumy900221@gmail.com>
2020-10-29 23:49:05 +0800

    phenomenon of creating new thread with both runnable and anonymous thread class

--- src/main/java/createthread/Main.java
+++ src/main/java/createthread/Main.java
@@ -7,10 +7,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        FirstTask firstTask = new FirstTask();
-        firstTask.start();
+        new Thread(() -> System.out.println("execute task from runnable!!!")) {
+            @Override
+            public void run() {
+                System.out.println("execute task from thread!!!");
+            }
+        }.start();
 
-        Thread thread = new Thread(new SecondTask());
-        thread.start();
+//        FirstTask firstTask = new FirstTask();
+//        firstTask.start();
+//
+//        Thread thread = new Thread(new SecondTask());
+//        thread.start();
     }
 }
